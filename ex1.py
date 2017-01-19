0807d64dfa692753de90bd1b46d21ae356b494c8
Young <abc.yry@163.com>
2017-01-19 10:18:48 +0800

    Python Exercise
    This is the place where I put all the python exercises.

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print "hello world!"
+print "hello again"
+list = ["hello world", "you need this", "to make a wish","and so on"]
+print(list[0:2])#this prints first two items of the list.
+print(list[1:3])#this print 2nd and 3rd item of the list.
+print(list[1]) #this prints the second item of the list
+print(list[0:3]) #this prints the first three items of the list.
