c2b8b398ff0d6df33d33eb20dcf7a97fddf476d6
Young <abc.yry@163.com>
2017-01-19 16:03:59 +0800

    lost lines

--- ex4.py
+++ ex4.py
@@ -1,6 +1,10 @@
 cars = 100
+space_in_a_car = 4 #if here is 4.0 then...
+drivers = 30
+passengers = 30
 cars_not_driven = cars - drivers
 cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car #here will be 120.0
 average_passengers_per_car = passengers / cars_driven
 
 
